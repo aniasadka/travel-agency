1635d38883341b836405ea7abf3ce31886c6e986
Anna Sadka <anna.sadka87@gmail.com>
2021-01-13 18:30:10 +0100

    17.5 - 3x use action dispatcher

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,21 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.addTag(tag);
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.removeTag(tag);
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeSearchDuration({
+      name: type,
+      value: value,
+    });
   }
 
   handleSearch(phrase){
