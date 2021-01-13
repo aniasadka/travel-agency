f28883c7bd12ed358ff333ca85f2169c15da07e2
Anna Sadka <anna.sadka87@gmail.com>
2021-01-13 18:20:57 +0100

    17.5 - impot other views to App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
+
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
+
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
