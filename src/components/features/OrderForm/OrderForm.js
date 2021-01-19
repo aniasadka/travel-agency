91101fe748c11462936864880ca3aa05aea7add1
Anna Sadka <anna.sadka87@gmail.com>
2021-01-19 22:46:52 +0100

    18.3 - Propsy w komponencie opcji

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import {Col, Row, Grid}  from 'react-flexbox-grid';
-import PageTitle from '../../common/PageTitle/PageTitle';
+
 import PropTypes from 'prop-types';
-
+import pricing from '../../../data/pricing.json';
+import PageTitle from '../../common/PageTitle/PageTitle';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
 
 
-const OrderForm = ({tripCost, options}) => (
+const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Grid>
-    <Row>
-      <Col xs={12}>
-        <PageTitle text='Trip options' />
-        <OrderSummary tripCost={tripCost} options={options} />
-      </Col>
-    </Row>
+      <Row>
+      {pricing.map(option => ( 
+          <Col md={4} key={option.id}>
+            <OrderOption currentValue={options[option.id]} setOrderOption={setOrderOption}/>
+          </Col>
+        ))};
+
+        <Col xs={12}>
+          <PageTitle text='Trip options' />
+          <OrderSummary tripCost={tripCost} options={options} />
+        </Col>
+      </Row>
   </Grid>
 );
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.any,
+  setOrderOption: PropTypes.func,
+};
 
 export default OrderForm;
--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,9 +1,17 @@
 import {connect} from 'react-redux';
 import OrderForm from './OrderForm';
 import {getOrderOptions} from '../../../redux/orderRedux';
+import {
+    getOrderOptions, 
+    setOrderOption,
+  } from '../../../redux/orderRedux';
 
 const mapStateToProps = state => ({
     options: getOrderOptions(state),
 });
 
+const mapDispatchToProps = dispatch => ({
+    setOrderOption: orderOption => dispatch(setOrderOption(orderOption)),
+});
+  
 export default connect(mapStateToProps)(OrderForm);
